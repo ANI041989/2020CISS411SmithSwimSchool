0fbadd4d4f8b3fc1bf3d60a722754d9cfe37c030
ANI041989 <73911092+ANI041989@users.noreply.github.com>
2020-11-12 15:03:56 -0600

    Add files via upload

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project1.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
