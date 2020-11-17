776c7b71a6d4c00a67ff1c0a7ca784140ab87f47
ianjmalexander <ianjmalexander@hotmail.com>
2020-11-16 18:29:18 -0600

    Coach Profile/Model/View/Controller finish

---
+++ Controllers/CoachController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Project1.Models;
+
+namespace Project1.Controllers
+{
+    [Authorize(Roles="Coach")]
+    public class CoachController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public CoachController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult AddProfile()
+        {
+            var currentUserId = this.User.FindFirst
+                (ClaimTypes.NameIdentifier).Value;
+            Coach coach = new Coach();
+            if (db.Coachs.Any(i => i.UserId ==
+            currentUserId))
+            {
+                coach = db.Coachs.FirstOrDefault
+                    (i => i.UserId == currentUserId);
+            }
+            else
+            {
+                coach.UserId = currentUserId;
+            }
+
+            return View(coach);
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddProfile
+            (Coach coach)
+        {
+            var currentUserId = this.User.FindFirst
+                (ClaimTypes.NameIdentifier).Value;
+            if (db.Coachs.Any
+                (i => i.UserId == currentUserId))
+            {
+                var coachToUpdate = db.Coachs.FirstOrDefault
+                    (i => i.UserId == currentUserId);
+                coachToUpdate.CoachName = coach.CoachName;
+                coachToUpdate.Bio = coach.Bio;
+                db.Update(coachToUpdate);
+            }
+            else
+            {
+                db.Add(coach);
+            }
+            await db.SaveChangesAsync();
+            return View("Index");
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -9,6 +9,7 @@ namespace Project1.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Coach> Coachs { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
