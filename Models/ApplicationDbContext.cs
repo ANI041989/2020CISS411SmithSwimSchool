a96f9952095b640a5df112d5cf17328f40a2d937
dapayne25@gmail.com <dapayne25@gmail.com>
2020-12-01 10:01:58 -0600

    SwimmerController SwimmerClass AddProfile SwimmerHome

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -12,7 +12,9 @@ namespace Project1.Models
         public DbSet<Coach> Coachs { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Lesson> Lessons { get; set; }
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		public object Swimmers { get; internal set; }
+
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
         }
