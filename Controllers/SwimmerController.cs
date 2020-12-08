a50a906e0aec37944a7a161586ec7cab0dbc26a8
dapayne25@gmail.com <dapayne25@gmail.com>
2020-12-08 11:19:31 -0600

    ProgressReport

--- Controllers/SwimmerController.cs
+++ Controllers/SwimmerController.cs
@@ -5,6 +5,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Project1.Models;
 
 namespace Project1.Controllers
@@ -38,6 +39,54 @@ namespace Project1.Controllers
             await db.SaveChangesAsync();
             return View(swimmer);
         }
+        public async Task<IActionResult> AllSession()
+		{
+            var session = await db.Sessions.Include
+                (s => s.Coach).ToListAsync();
+            return View(session);
+		}
+        public async Task<IActionResult> EnrollSession(int id)
+        {
+            var currentUserId = this.User.FindFirst
+               (ClaimTypes.NameIdentifier).Value;
+            var swimmerId = db.Swimmers.FirstOrDefault
+                (s => s.UserId == currentUserId).SwimmerId;
+            Enrollment enrollment = new Enrollment
+            {
+                SessionId = id,
+                SwimmerId = swimmerId
+            };
+            db.Add(enrollment);
+            var session = await db.Sessions.FindAsync
+                (enrollment.SessionId);
+
+            await db.SaveChangesAsync();
+            return View("SwimmmerHome");
+        }
+        public async Task<IActionResult> CheckGrade()
+		{
+            ClaimsPrincipal currentUser = this.User;
+            var currentUserId = currentUser.FindFirst
+                (ClaimTypes.NameIdentifier).Value;
+            if (currentUserId == null)
+			{
+                return NotFound();
+			}
+            var swimmer = await db.Swimmers
+                .SingleOrDefaultAsync
+                (s => s.UserId == currentUserId);
+            var swimmerId = swimmer.SwimmerId;
+            var allSessions = await db.Enrollments
+                .Include(e => e.Session).Where
+                (c => c.SwimmerId == swimmerId)
+                .ToListAsync();
+            if (allSessions == null)
+			{
+                return NotFound();
+			}
+            ViewData["sname"] = swimmer.SwimmerName;
+            return View(allSessions);
+		}
         public IActionResult Index()
         {
             return View();
